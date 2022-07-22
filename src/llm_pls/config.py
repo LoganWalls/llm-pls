b2149ab379f5d22b81229e5a8bb792deec9bf539
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2022-07-22 16:05:58 -0400

    allow config via env variables

--- src/llm_pls/config.py
+++ src/llm_pls/config.py
@@ -1,8 +1,14 @@
-port = 8081
-model_name = "bigscience/bloom"
-model_device = "auto"
+import os
+
+port = int(os.getenv("LLMPLS_PORT", 8081))
+model_name = os.getenv("LLMPLS_MODEL", "gpt2")
+
+model_device = os.getenv("LLMPLS_DEVICE", "auto")
+if model_device.isnumeric():
+    model_device = int(model_device)
+
 input_device = 0 if model_device == "auto" else model_device
-debug = True
+debug = bool(os.getenv("LLMPLS_DEBUG", False))
 
 if debug:
     model_device = "cpu"
